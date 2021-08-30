89f1d8cb2c25930978a4b83d516ad43d8d6fa241
Dico Diaz <dicodiaz@gmail.com>
2021-08-30 15:11:45 -0500

    Add cancel button and cancel function to menu

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-let hamButton = document.getElementById('hamb-icon');
-let menuSection = document.getElementById('m-menu');
+let hamButton = document.getElementById("hamb-icon");
+let menuSection = document.getElementById("m-menu");
+let cancelButton = document.getElementById("cancel-icon");
 
-hamButton.addEventListener('click', function() {
-    menuSection.classList.remove('d-none');
+hamButton.addEventListener("click", function () {
+  menuSection.classList.remove("d-none");
+});
+
+cancelButton.addEventListener("click", function () {
+  menuSection.classList.add("d-none");
 });
