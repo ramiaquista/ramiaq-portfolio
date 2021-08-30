4110c0e33336fd0a30f0f39073cfd951dbd554e1
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-08-30 17:25:46 -0300

    Add menu options to mobile-menu

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 let hamButton = document.getElementById("hamb-icon");
 let menuSection = document.getElementById("m-menu");
 let cancelButton = document.getElementById("cancel-icon");
+let menuOptions = document.querySelectorAll("#options-list > li");
+
+for (let option of menuOptions) {
+  option.addEventListener("click", function () {
+    menuSection.classList.add("d-none");
+  });
+}
 
 hamButton.addEventListener("click", function () {
   menuSection.classList.remove("d-none");
