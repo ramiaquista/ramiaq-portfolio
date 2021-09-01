509f6a7670bf0827043860b350c5b2fa88fa9161
Dico Diaz <dicodiaz@gmail.com>
2021-09-01 14:46:32 -0500

    Add error message

--- form-validator.js
+++ form-validator.js
@@ -1,10 +1,15 @@
-let form = document.getElementById('contact-form');
-let emailInput = document.getElementById('email-input');
-
+const form = document.getElementById('contact-form');
+const emailInput = document.getElementById('email-input');
+const msg = document.getElementById('error-msg');
+const ERROR_MSG = 'The email address has to be in lowercase.';
 
 form.addEventListener('submit', (event) => {
-    if (emailInput.value !== emailInput.value.toLowerCase()) {
-        alert('Email Incorrect');
-        event.preventDefault();
-    }
-})
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
+    msg.innerText = ERROR_MSG;
+    emailInput.classList.add('error-border');
+    event.preventDefault();
+  } else {
+    msg.innerText = '';
+    emailInput.classList.remove('error-border');
+  }
+});
