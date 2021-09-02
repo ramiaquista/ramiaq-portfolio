dabd990e5883b205b757be25ba0eac3956e1acd8
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-09-02 14:20:50 -0300

    Add local storage to preserve form data

---
+++ form-storage.js
@@ -0,0 +1,17 @@
+const formStorage = document.getElementById('contact-form');
+const nameStorage = formStorage.elements.name;
+const emailStorage = formStorage.elements.email;
+const msgStorage = formStorage.elements.msg;
+
+function populateStorage() {
+  const formData = {
+    name: nameStorage.value,
+    email: emailStorage.value,
+    msg: msgStorage.value,
+  };
+  localStorage.setItem('formData', JSON.stringify(formData));
+}
+
+nameStorage.onchange = populateStorage;
+emailStorage.onchange = populateStorage;
+msgStorage.onchange = populateStorage;
