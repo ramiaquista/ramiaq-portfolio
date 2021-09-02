ec254f7cc26ad00573c40acf8786b9a3b6004fdd
Ramiro Aquistapace <ramiaquista70@gmail.com>
2021-09-02 15:26:01 -0300

    Fix eslint errors

--- form-validator.js
+++ form-validator.js
@@ -13,13 +13,3 @@ form.addEventListener('submit', (event) => {
     emailInput.classList.remove('error-border');
   }
 });
-
-nameStorage.onchange = populateStorage;
-emailStorage.onchange = populateStorage;
-msgStorage.onchange = populateStorage;
-
-var storageData = JSON.parse(localStorage.getItem('formData'));
-
-nameStorage.value = storageData.name;
-emailStorage.value = storageData.email;
-msgStorage.value = storageData.msg;
